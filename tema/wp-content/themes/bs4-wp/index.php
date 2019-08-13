530bce36c03220bf20120c1e9805fbffba8f6175
Anne Kelly <annekelly533@gmail.com>
2019-08-13 01:04:07 -0300

    Comecando a personalizar o tema e dividir o index em Header, Footer e Sidebar

---
+++ tema/wp-content/themes/bs4-wp/index.php
@@ -0,0 +1,112 @@
+<?php get_header(); ?>
+
+    <div class="row">
+
+        <div class="col-cm-12 col-md-4 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="assets/img/kombi.jpg" alt="mão usando celular">
+                <div class="card-body">
+                    <h5 class="card-title mb-4">
+                        Lorem ipsum dolor sit amet
+                    </h5>
+                    <a href="" class="btn btn-my-color-1 dark">Leia mais</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-cm-12 col-md-4 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="assets/img/kombi.jpg" alt="mão usando celular">
+                <div class="card-body">
+                    <h5 class="card-title mb-4">
+                        Lorem ipsum dolor sit amet
+                    </h5>
+                    <a href="" class="btn btn-my-color-1 dark">Leia mais</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-cm-12 col-md-4 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="assets/img/kombi.jpg" alt="mão usando celular">
+                <div class="card-body">
+                    <h5 class="card-title mb-4">
+                        Lorem ipsum dolor sit amet
+                    </h5>
+                    <a href="" class="btn btn-my-color-1 dark">Leia mais</a>
+                </div>
+            </div>
+        </div>
+
+    </div>  
+
+      <div class="row">
+        <div class="col-md-8 col-sm-12">
+          <div class="blog-post">
+            <h3 class="mb-3 pb-2 border-bottom"><a href="">Lorem ipsum dolor sit amet</a></h3>
+
+            <div class="row">
+              <div class="col-md-12 col-lg-6 mb-3">
+                <img src="assets/img/kombi.jpg" class="img-fluid rounded" alt="kombizinha bonita">
+              </div>
+              <div class="col-md-12 col-lg-6 mb-3">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum;</p>
+              </div>
+            </div>
+            <p class="text-muted">Publicado em: <span class="badge badge-my-color-2">25/07/2019</span></p>
+          </div>
+
+          <div class="blog-post">
+            <h3 class="mb-3 pb-2 border-bottom"><a href="">Lorem ipsum dolor sit amet</a></h3>
+
+            <div class="row">
+              <div class="col-md-12 col-lg-6 mb-3">
+                <img src="assets/img/kombi.jpg" class="img-fluid rounded" alt="kombizinha bonita">
+              </div>
+              <div class="col-md-12 col-lg-6 mb-3">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum;</p>
+              </div>
+            </div>
+            <p class="text-muted">Publicado em: <span class="badge badge-my-color-2">25/07/2019</span></p>
+          </div>
+
+          <div class="blog-post">
+            <h3 class="mb-3 pb-2 border-bottom"><a href="">Lorem ipsum dolor sit amet</a></h3>
+
+            <div class="row">
+              <div class="col-md-12 col-lg-6 mb-3">
+                <img src="assets/img/kombi.jpg" class="img-fluid rounded" alt="kombizinha bonita">
+              </div>
+              <div class="col-md-12 col-lg-6 mb-3">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum;</p>
+              </div>
+            </div>
+            <p class="text-muted">Publicado em: <span class="badge badge-my-color-2">25/07/2019</span></p>
+          </div>
+
+          <div class="blog-post">
+            <h3 class="mb-3 pb-2 border-bottom"><a href="">Lorem ipsum dolor sit amet</a></h3>
+
+            <div class="row">
+              <div class="col-md-12 col-lg-6 mb-3">
+                <img src="assets/img/kombi.jpg" class="img-fluid rounded" alt="kombizinha bonita">
+              </div>
+              <div class="col-md-12 col-lg-6 mb-3">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum;</p>
+              </div>
+            </div>
+            <p class="text-muted">Publicado em: <span class="badge badge-my-color-2">25/07/2019</span></p>
+          </div>
+
+          <div class="blog-pagination mb-5">
+            <a href="" class="btn btn-outline-my-color-1">Mais antigos </a> <a href="" class="btn btn-outline-my-color-1">Mais novos</a>
+          </div>
+
+        </div>
+
+        <?php get_sidebar(); ?>
+        
+      </div>
+    </div>
+
+<?php get_footer(); ?>
---
+++ tema/wp-content/themes/bs4-wp/sidebar.php
@@ -0,0 +1,38 @@
+<div class="col-md-4 col-sm-12">
+          <div class="card mb-4">
+            <h5 class="card-header">Categorias</h5>
+            <div class="card-body">
+              <ul>
+                <li><a href="">Projetos</a></li>
+                <li><a href="">Novidades</a></li>
+                <li><a href="">Planos</a></li>
+              </ul>
+            </div>
+          </div>
+          
+          <div class="card mb-4">
+            <h5 class="card-header">Últimos posts</h5>
+            <div class="card-body">
+              <ul>
+                <li><a href="">Loren ipsum dolor</a></li>
+                <li><a href="">Loren ipsum dolor Loren ipsum dolor</a></li>
+                <li><a href="">Loren ipsum dolor Loren ipsum dolor Loren ipsum dolor</a></li>
+                <li><a href="">Loren ipsum dolor Loren ipsum dolor Loren ipsum</a></li>
+                <li><a href="">Loren ipsum</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="card mb-4">
+              <h5 class="card-header">Links</h5>
+              <div class="card-body">
+                <ul>
+                  <li><a href="">Instagram</a></li>
+                  <li><a href="">Twitter</a></li>
+                  <li><a href="">Facebook</a></li>
+                  <li><a href="">Linkedin</a></li>
+                </ul>
+              </div>
+            </div>
+
+        </div>
