8da6acb79b9ee35879196cdb278825055ce59202
Anne Kelly <annekelly533@gmail.com>
2019-08-13 01:41:56 -0300

    Automatizando o navbar - começo

--- tema/wp-content/themes/bs4-wp/function.php
+++ tema/wp-content/themes/bs4-wp/function.php
@@ -16,4 +16,19 @@ if (function_exists('_wp_render_title_tag')) {
     add_action('wp_head', 'bs4wp_render_title');
 }
 
+// Registra o custom navigation walker
+if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+	// file does not exist... return an error.
+	return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+} else {
+	// file exists... require it.
+    require_once get_template_directory . '/class-wp-bootstrap-navwalker.php';
+}
+
+//Registrar os Menus
+register_nav_menus( array(
+    'principal' => __('Menu principal', 'bs4-wp')
+));
+
+
 ?>
